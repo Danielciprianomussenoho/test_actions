2fc2966565e62ab9076d54e4f52336f8ddf9d198
Daniel cipriano mussenoho <Danielciprianomussenoho@gmail.com>
2025-12-26 02:53:19 +0000

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
 
   return (
     <div className='flex flex-col gap-2 items-center justify-center h-screen'>
-      <h1 className='text-2xl font-semibold mb-4 bg-gray-400 p-2 rounded-2xl text-white'>Counter: {count}</h1>
+      <h1 className='text-2xl font-semibold mb-4 bg-gray-400 p-2 rounded-2xl text-white'>Counters: {count}</h1>
       <div className=" flex gap-2 justify-center items-center">
         <button className="bg-green-600 p-2 rounded-2xl text-white" onClick={()=>dispatch(increment())}>increment</button>
         <button className="bg-red-600 p-2 rounded-2xl text-white" onClick={()=>dispatch(decrement())}>dencrement</button>
